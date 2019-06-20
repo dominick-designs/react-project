f7d18e350aac7bfc9c2c554a43a00cd9e7e742ff
Dominick Designs <dominick@dominickdesigns.space>
2019-06-20 10:44:52 -0400

    commit 4

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const Pet = () => {
+    return React.
+}
+
+const App = () => {
+        return React.createElement(
+            "div", {}, [
+            React.createElement("h1", {}, 
+                "Adopt Me!")
+                React.createElement(Pet)
+                React.createElement(Pet)
+            ])
+}
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
+
+
