6aea65eadf762e7afe29fd0ef04969cdc8378d97
Dominick Designs <dominick@dominickdesigns.space>
2019-06-20 13:39:32 -0400

    add all using period

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-const Pet = () => {
-    return React.
+const Pet = props => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed)
+    ])
 }
 
 const App = () => {
-        return React.createElement(
-            "div", {}, [
-            React.createElement("h1", {}, 
-                "Adopt Me!")
-                React.createElement(Pet)
-                React.createElement(Pet)
-            ])
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {name: "Tucker", animal:"Dog", breed: "Mutt"}),
+        React.createElement(Pet, {name: "Dash", animal:"Dog", breed: "Mutt"}),
+        React.createElement(Pet, {name: "Tootles", animal:"Cat", breed: "Tabby"}),
+        React.createElement(Pet, {name: "Freddy", animal:"Frog", breed: "American Bullfrog"})
+    ])
 }
+
 ReactDOM.render(React.createElement(App), document.getElementById("root"))
-
-
